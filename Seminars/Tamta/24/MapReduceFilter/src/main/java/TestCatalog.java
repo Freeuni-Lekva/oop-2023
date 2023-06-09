1d68c4b6880f04565fb5cea3ad98e3e0fb147c02
tamtatop <ttopu18@freeuni.edu.ge>
2023-06-09 14:34:27 +0400

    feat: add tests

---
+++ Seminars/Tamta/24/MapReduceFilter/src/main/java/TestCatalog.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestCatalog {
+
+    private static EmployeeCatalog catalog;
+
+    @BeforeAll
+    public static void init(){
+        List<Employee> employeesList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++){
+            int salary = new Random().nextInt(900) + 100;
+            employeesList.add(new Employee("f","s", Employee.Department.INFRASTRUCTURE, salary));
+        }
+        employeesList.add(new Employee("Elon","Musk", Employee.Department.INFRASTRUCTURE, 1100));
+        employeesList.add(new Employee("Elon","Musk1", Employee.Department.INFRASTRUCTURE, 90));
+        employeesList.add(new Employee("Erekle","Meore", Employee.Department.DATA_SCIENCE, 100000));
+
+        catalog = new EmployeeCatalog(employeesList);
+
+    }
+
+    @Test
+    public void findEmployeeWithHighestSalary(){
+        assertEquals(catalog.findEmployeeWithHighestSalary().getSalary(), 100000);
+    }
+
+    @Test
+    public void getSalaryGapInDepartment(){
+        assertEquals(catalog.getSalaryGapInDepartment(Employee.Department.INFRASTRUCTURE), 1010);
+    }
+
+    @Test
+    public void findDepartmentWithHighestCumulativeSalary(){
+        assertEquals(catalog.findDepartmentWithHighestCumulativeSalary(), Employee.Department.DATA_SCIENCE);
+    }
+}
